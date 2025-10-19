dedbc170f4f6173b733e7669863b64f849214d75
Ilnaz11 <Baimukhametovilnaz@gmail.com>
2025-10-19 14:30:33 +0500

    Initial commit

---
+++ src/test/java/ru/baymukhametov/TaskTrackerPro/TaskTrackerProApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.baymukhametov.TaskTrackerPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
