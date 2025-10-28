574de0d23f73093eefe6f1670b605f42652fced8
Ilnaz11 <Baimukhametovilnaz@gmail.com>
2025-10-28 12:21:14 +0500

    N8: Добавил методы в UserController

--- src/main/java/ru/baymukhametov/TaskTrackerPro/Controller/UserController.java
+++ src/main/java/ru/baymukhametov/TaskTrackerPro/Controller/UserController.java
@@ -1,14 +1,13 @@
 package ru.baymukhametov.TaskTrackerPro.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.baymukhametov.TaskTrackerPro.Entity.User;
 import ru.baymukhametov.TaskTrackerPro.Service.UserService;
+import ru.baymukhametov.TaskTrackerPro.dto.UserRequestDto;
 import ru.baymukhametov.TaskTrackerPro.dto.UserResponseDto;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -28,5 +27,21 @@ public class UserController {
     public List<UserResponseDto> getAllUsers() {
         return userService.getAllUsers();
     }
+
+    @DeleteMapping
+    public void deleteUser(Long id) {
+        userService.deleteUser(id);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<UserResponseDto> findById(Long id) {
+        return userService.findById(id);
+    }
+
+    @PutMapping("/{id}/update")
+    public UserResponseDto updateUser(Long id, UserRequestDto userRequestDto) {
+        return userService.updateUser(id, userRequestDto);
+    }
+
 
 }
