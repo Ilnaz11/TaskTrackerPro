53ee721c0cdf517eeea38c4f3f886dac2dd0993b
Ilnaz11 <Baimukhametovilnaz@gmail.com>
2025-10-28 17:00:53 +0500

    N9: Добавил методы в TaskController,и добавил метод toDtoList

--- src/main/java/ru/baymukhametov/TaskTrackerPro/Controller/TaskController.java
+++ src/main/java/ru/baymukhametov/TaskTrackerPro/Controller/TaskController.java
@@ -1,25 +1,46 @@
 package ru.baymukhametov.TaskTrackerPro.Controller;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.baymukhametov.TaskTrackerPro.Entity.Task;
 import ru.baymukhametov.TaskTrackerPro.Repository.TaskRepository;
+import ru.baymukhametov.TaskTrackerPro.Service.TaskService;
+import ru.baymukhametov.TaskTrackerPro.dto.TaskCreateDto;
+import ru.baymukhametov.TaskTrackerPro.dto.TaskResponseDto;
+import ru.baymukhametov.TaskTrackerPro.dto.TaskStatusUpdateDto;
 import ru.baymukhametov.TaskTrackerPro.mapper.TaskMapper;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/tasks")
 @RestController
 public class TaskController {
 
-    private final TaskRepository taskRepository;
+    private final TaskService taskService;
     private final TaskMapper taskMapper;
 
     @PostMapping
-    public
+    public TaskResponseDto createTask(Task task) {
+        return taskMapper.toDto(task);
+    }
+
+    @GetMapping
+    public List<TaskResponseDto> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public TaskResponseDto updateTask(@PathVariable Long id,
+                                      @RequestBody TaskCreateDto taskCreateDto,
+                                      @RequestBody TaskStatusUpdateDto taskStatusUpdateDto) {
+        return taskService.updateTask(id, taskCreateDto, taskStatusUpdateDto);
+    }
 
 }
-//o	создать задачу,
-//o	получить все задачи,
-//o	обновить статус задачи,
-//o	удалить задачу.
--- src/main/java/ru/baymukhametov/TaskTrackerPro/dto/TaskResponseDto.java
+++ src/main/java/ru/baymukhametov/TaskTrackerPro/dto/TaskResponseDto.java
@@ -1,4 +1,12 @@
 package ru.baymukhametov.TaskTrackerPro.dto;
 
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class TaskResponseDto {
+    private String title;
+    private String description;
+    private LocalDateTime dueDate;
 }
